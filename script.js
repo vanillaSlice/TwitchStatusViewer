c1b107ffeec9ac36bc53edae0445bee3c6284b95
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-06-13 18:10:39 +0100

    Plugged in to Twitch API

--- script.js
+++ script.js
@@ -1,46 +1,113 @@
-//$(document).ready(function () {
-//  
-//  "use strict";
-//  
-//  var originalApiEndpoint = "https://api.twitch.tv/kraken";
-//  var updatedApiEndpoint = "https://wind-bow.glitch.me/twitch-api";
-//  
-//  function getRequest(query, id, success, error) {
-//    $.getJSON(updatedApiEndpoint + "/" + query + "/" + id, success, error);
-//  }
-//  
-//  function getStream(id, success, error) {
-//    getRequest("streams", id, success, error);
-//  }
-//  
-//  function getChannel(id, success, error) {
-//    getRequest("channels", id, success, error);
-//  }
-//  
-//  function getUser(id, success, error) {
-//    getRequest("users", id, success, error);
-//  }
-//  
-//  function showImage(data) {
-//    if (data.stream !== null) {
-//      var img = $("<img>").attr("src", data.stream.channel.logo).addClass("img-circle");
-//      $(document.body).append(img);
-//    }
-//  }
-//  
-//  function showError(error) {
-//    console.error(error);
-//  }
-//  
-//  getStream("freecodecamp", showImage, showError);
-//  getStream("ESL_SC2", showImage, showError);
-//  
-//});
-
 $(document).ready(function () {
 
-  $("input:radio").change(function (event) {
-    console.log(event.target.value);
-  })
+  "use strict";
+
+  var apiEndpoint = "https://wind-bow.glitch.me/twitch-api",
+    streamers = [
+      "freecodecamp",
+      "ESL_SC2",
+      "OgamingSC2",
+      "brunofin",
+      "cretetion",
+      "storbeck",
+      "habathcx",
+      "RobotCaleb",
+      "comster404",
+      "noobs2ninjas"
+    ],
+    missingLogoName = "missing-logo.png";
+
+  streamers.forEach(makeChannelRequest);
+
+  function makeChannelRequest(id) {
+    $.getJSON(apiEndpoint + "/channels/" + id, channelRequestCallback);
+
+    function channelRequestCallback(data) {
+      var channelInformation = getChannelInformation(id, data);
+      makeStreamRequest(channelInformation);
+    }
+  }
+
+  function getChannelInformation(id, data) {
+    var channelInformation = {
+      id: id
+    };
+
+    if (data.hasOwnProperty("error")) {
+      channelInformation.status = "closed";
+      channelInformation.href = "#";
+      channelInformation.logo = missingLogoName;
+      channelInformation.longDescription = getChannelErrorDescription(data);
+      channelInformation.shortDescription = channelInformation.longDescription;
+    } else {
+      channelInformation.href = data.url;
+      channelInformation.logo = data.logo || missingLogoName;
+    }
+
+    return channelInformation;
+  }
+
+  function getChannelErrorDescription(data) {
+    switch (data.status) {
+      case 404:
+        return "Channel does not exist";
+      case 422:
+        return "Channel is unavailable";
+      default:
+        return "Could not retrieve channel information";
+    }
+  }
+
+  function makeStreamRequest(channelInformation) {
+    if (channelInformation.status === "closed") {
+      addChannelInformationToDOM(channelInformation);
+    } else {
+      $.getJSON(apiEndpoint + "/streams/" + channelInformation.id, streamRequestCallback);
+    }
+
+    function streamRequestCallback(data) {
+      addStreamInformation(data, channelInformation);
+      addChannelInformationToDOM(channelInformation);
+    }
+  }
+
+  function addStreamInformation(data, channelInformation) {
+    if (data.stream === null) {
+      channelInformation.status = "offline";
+      channelInformation.longDescription = "Offline";
+      channelInformation.shortDescription = channelInformation.longDescription;
+    } else {
+      channelInformation.status = "online";
+      channelInformation.shortDescription = data.stream.channel.game;
+      channelInformation.longDescription = channelInformation.shortDescription +
+        ": " + data.stream.channel.status;
+    }
+  }
+
+  function addChannelInformationToDOM(channelInformation) {
+    var a = "<a href='" + channelInformation.href + "' " +
+      "target='_blank' class='" + channelInformation.status + " clearfix'>" +
+      "<span class='col-xs-2 text-center'>" +
+      "<img src='" + channelInformation.logo + "' class='img-circle' " +
+      "alt='" + channelInformation.id + " logo' title='" +
+      channelInformation.id + "'>" +
+      "</span>" +
+      "<span class='col-xs-10'>" +
+      "<strong class='col-xs-12 col-sm-2 text-center'>" +
+      channelInformation.id + "</strong>" +
+      "<span class='col-xs-12 col-sm-9 col-sm-offset-1 text-center'>" +
+      "<span class='hidden-xs'>" + channelInformation.longDescription +
+      "</span>" +
+      "<span class='hidden-sm hidden-md hidden-lg'>" +
+      channelInformation.shortDescription + "</span>" +
+      "</span>" +
+      "</span>" +
+      "</a>";
+    $("main").append(a);
+  }
+
+  //  $("input:radio").change(function (event) {
+  //    console.log(event.target.value);
+  //  });
 
 });
